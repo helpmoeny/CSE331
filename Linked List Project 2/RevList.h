f168f98320e03f49344c5335b2e5c456a3eebd9a
unknown <asdfghjkl1234567890zz@gmail.com>
2015-10-02 16:03:51 -0400

    Replacing my old files, Still need finishing up and clean up. More work to do

--- Linked List Project 2/RevList.h
+++ Linked List Project 2/RevList.h
@@ -5,165 +5,403 @@
 
 #ifndef REVLIST_H
 #define REVLIST_H
-#include <iostream>
 
-template<class T> class CRevList{
+//#include <iostream>
 
-	public:
-	// Constructor.  Sets to an empty list.
-	CRevList()
-	{
-		
-	}
+template<class T> class CRevList
+{
 
-	// Destructor.  Deletes everything in the list.
-	~CRevList(){
-		
-	}
+public:
+  // Constructor.  Sets to an empty list.
+  CRevList() 
+  {
+      m_head = NULL;
+      m_tail = NULL;
+      reversed = false;
+  }
+
+  // Destructor.  Deletes everything in the list.
+  ~CRevList()
+  {
+      Node *itr = m_head;
+      
+      while (itr != NULL)
+      {
+          Node *temp = itr;
+          
+          if (!reversed)
+          {
+              itr = itr->m_next;
+          }
+          
+          else
+          {
+              itr = itr->m_prev;
+          }
+          
+          delete temp;
+      }
+      
+      m_head = NULL;
+      m_tail = NULL;
+  }
+
+  // Copy constructor.  Copies another list.
+  CRevList(const CRevList &b)
+  {
+      Node *itr = b->m_head;
+      
+      while (itr != NULL)
+      {
+          PushBack(itr->m_payload);
+          
+          if (!b.IsReversed())
+          {
+              itr = itr->m_next;
+          }
+          
+          else
+          {
+              itr = itr->m_prev;
+          }
+      }
+  }
+
+  // Assignment operator.  Copies another list.
+  void operator=(const CRevList &b);
+
+  // Clear the list.
+  void Clear()
+  {
+      Node *itr = m_head;
+      
+      while (itr != NULL)
+      {
+          Node *temp = itr;
+          
+          if (!reversed)
+          {
+             itr = itr->m_next;
+          }
+          
+          else
+          {
+             itr = itr->m_prev;
+          }
+          
+          delete temp;
+      }
+      
+      m_head = NULL;
+      m_tail = NULL;
+      
+      
+  }
 
 
-	// Copy constructor.  Copies another list.
-	CRevList(const CRevList &b){
-		
-	}
+  //
+  // class Node
+  // The nested node class for objects in our linked list.
+  //
+  // YOU MAY NOT MODIFY THIS CLASS!
+  class Node 
+  {
+  public:
+    friend class CRevList;
+  
+    Node() {m_next = 0;  m_prev = 0;}
+    Node(const T &t) {m_payload = t;  m_next = 0;  m_prev = 0;}
+  
+    T Data() {return m_payload;}
+    const T Data() const {return m_payload;}
+  
+  private:
+    Node    *m_next;
+    Node    *m_prev;
+    T       m_payload;
+  };
+  
+  // Determines if the list is empty
+  bool IsEmpty() const
+  {
+      if (m_head == NULL || m_tail == NULL)
+      {
+          return true;
+      }
+      
+      return false;
+  }
 
-	// Assignment operator.  Copies another list.
-	void operator=(const CRevList &b);
+  //methods to add data to the front or the back of the list
+  void PushFront(const T &t)
+  {
+      /* Node *new_node = new Node(t); //Pushback original
+      
+	  if (m_first == false)
+      {
+		  n = new Node();
+		  n ->m_payload = t;
+		  n ->m_next = n;
+		  n ->m_prev = n;
+		  n -> m_next = m_head;
+          m_head = n;
+          m_tail = n;
+		  m_first = true;
+		  return;
+      }
+		  n = new Node();
+		  n ->m_payload = t;
+		  n -> m_prev = m_tail;
+		  m_tail -> m_next = n;
+		  m_head -> m_prev = n;
+		  n -> m_next = m_head;
+          m_head = n;
+          m_tail = n; */
+		  
+  }
+  
+  void PushBack(const T &t)
+  {
+      Node *node = new Node(t);
+      if(IsEmpty());
+	  {
+		  m_head.m_next = node;
+		  node -> m_next = m_head.m_next;
+		  node -> m_prev = m_head.m_next;
+	  }
+	  else{
+		  Begin()-> p_prev-> m_next = node;
+		  node -> m_prev = End();
+		  node -> m_next = Begin();
+		  Begin() -> m_prev = node;
+	  }
+	  m_size++;
+	  
+      /* if (m_first == false)
+      {
+		  n = new Node(t);
+		  //n ->m_payload = t;
+		  n ->m_next = n;
+		  n ->m_prev = n;
+		  n -> m_next = m_head;
+          m_head = n;
+          m_tail = n;
+		  m_first = true;
+		  return;
+      }
+		  n = new Node();
+		  n -> m_prev = m_tail;
+		  m_tail -> m_next = n;
+		  m_tail = n; */
 
-	// Clear the list.
-	void Clear(){
-	Node *temp = m_head;
-		while (temp != NULL){
-			m_head = m_head.m_next;
-			Delete(temp);
-			temp = m_head;
-		}
-		m_head, m_tail = NULL;
-	}
+  }
+  
+  void PopFront()
+  {
+     Node *second = Next(Begin());
+	 Delete(Begin());
+	 m_head.m_next = second;
+  }
+  
+  void PopBack()
+  {
+	 Delete(End());
+  }
 
+  //Get a pointer to the first node in the list
+  const Node *Begin() const{return m_head;}	//return m_head.m_next;
+  Node *Begin(){return m_head;}				//same...
 
-	//
-	// class Node
-	// The nested node class for objects in our linked list.
-	//
-	// YOU MAY NOT MODIFY THIS CLASS!
-	class Node{
-		public:
-		friend class CRevList;
+  //get a pointer to the last node in the list
+  const Node *End() const{return m_tail;}	//return m_head.m_next->m_prev;
+  Node *End(){return m_tail;}
 
-		Node() { m_next = 0;  m_prev = 0; }
-		Node(const T &t) { m_payload = t;  m_next = 0;  m_prev = 0; }
+  //get a pointer to node next in the list
+  const Node *Next(const Node *n) const
+  {
+      if (!reverse!reversed)
+      {
+          return n->m_next;
+      }
 
-		T Data() { return m_payload; }
-		const T Data() const { return m_payload; }
+      else
+      {
+          return n->m_prev;
+      }
+      
+  }
+  
+  Node *Next(const Node *n)
+  {
+      if (!reversed)
+      {
+          return n->m_next; //Don't know if we need the Return in front of these
+      }
 
-		private:
-		Node    *m_next;
-		Node    *m_prev;
-		T       m_payload;
-	};
+      else
+      {
+          return n->m_prev; //Don't know if we need the Return in front of these
+      }
+  }
 
-	// Determines if the list is empty
-	bool IsEmpty() const { return (size == 0) ? true : false; }
+  //Find a node with the specified key
+  const Node *Find(const T &t) const
+  {
+      Node *itr = m_head;
+      
+      while (itr != NULL)
+      {
+          if (t == itr->m_payload)
+          {
+              return itr;
+          }
+          
+          if (!reversed)
+          {
+              itr = itr->m_next;
+          }
+          
+          else
+          {
+              itr = itr->m_prev;
+          }
+      }
+      
+      return NULL;
+  }
+  
+  Node *Find(const T &t)
+  {
+      
+      Node *itr = m_head;
+      
+      while (itr != NULL)
+      {
+          if (t == itr->m_payload)
+          {
+              return itr;
+          }
+          
+          if (!reversed)
+          {
+              itr = itr->m_next;
+          }
+          
+          else
+          {
+              itr = itr->m_prev;
+          }
+      }
+      
+      return NULL;
+  }
 
-	//methods to add data to the front or the back of the list
-	void PushFront(const T &t) {
-		Node *temp = new Node(t);
-		cout << t << endl;
-
-		if (size > 0){
-			temp->m_next = &m_head;
-			temp->m_prev = m_head.m_prev;
-			m_head.m_prev->m_next = temp;
-			m_head.m_prev = temp;
-			m_head = *temp;
+  //Delete the given node
+  void Delete(Node *n)
+  {
+	  if(n == nullptr){return;}
+	  if(n == m_head){
+		if(n->m_next == n){
+			m_head = nullptr;
+			m_tail = nullptr;
 		}
 		else{
-			m_head = *temp;
-			temp->m_next = &m_head;
-			temp->m_prev = &m_head;
+			if(reversed){
+				m_head = n->m_prev;
+			}
+			else{
+				m_head = n->m_next;
+			}
 		}
-		size++;
-	}
-
-	void PushBack(const T &t) {
-		Node *temp = new Node(t);
-		cout << t << endl;
-
-		if (size > 0){
-			temp->m_next = &m_head;
-			temp->m_prev = m_head.m_prev;
-			m_head.m_prev->m_next = temp; //ISSUE IS HERE TRYING TO SET OLD TAIL.NEXT TO NEW TAIL.
-			m_head.m_prev = temp;
+	  }
+	  if(n == m_tail){
+		if(n->m_prev == n){
+			m_tail = nullptr;
+			m_head = nullptr;
 		}
 		else{
-			m_head = *temp;
-			temp->m_next = &m_head;
-			temp->m_prev = &m_head;
+			if(reversed){
+				m_tail = n->m_next;
+			}
+			else{
+				m_tail = n->m_prev;
+			}
 		}
-		size++;
-	}
+	  }
+      n->m_prev->m_next = n->m_next;
+      n->m_next->m_prev = n->m_prev;
+	  
+      delete n;
+  }
 
-	void PopFront() {
-		*m_head.m_prev->m_next = m_head.m_next;
-		*m_head.m_next->m_prev = m_head.m_prev;
-		Node temp = m_head;
-		m_head = m_head->m_next;
-		Delete(temp);
-		size--;
-	}
+  //Reverse the list in O(1) time
+  void Reverse()
+  {
+      /* Node *temp = m_head;
+      m_head = m_tail;
+      m_tail = temp;
+      reversed = !reversed; */
+	  Node *temp = m_head;
+	  Node *nextnode;
+	  Node *head = m_head;
+	  
+	  nextnode = head -> m_next;
+	  temp = head -> m_prev;
+	  head -> m_prev = head -> m_next;
+	  head -> m_next = temp;
+	  head = nextnode;
+	  
+	  while(head != m_head){
+		nextnode = head -> m_next;
+		temp = head -> m_prev;
+		head -> m_prev = head -> m_next;
+		head = nextnode;
+	  }
+  }
+  
+  bool IsReversed()
+  {
+      return reversed;
+  }
 
-	void PopBack() {
-		Node m_tail = m_head->m_prev;
-		*m_head.m_prev = m_tail.m_prev;
-		*m_tail.m_prev->m_next = m_head;
-		Delete(m_tail);
-		size--;
-	}
+  
 
-	//Get a pointer to the first node in the list
-	const Node *Begin() const { return IsEmpty() ? NULL : &m_head; }
-	Node *Begin() { return IsEmpty() ? NULL : &m_head; }
+private:
 
-	//get a pointer to the last node in the list
-	const Node *End() const { return IsEmpty() ? NULL : &m_tail; }
-	Node *End() { return IsEmpty() ? NULL : &m_tail; }
+  //NOTE: you may add any private member variables or 
+  //      methods you need to complete the implementation
 
-	//get a pointer to node next in the list
-	const Node *Next(const Node *n) const { return &n.m_next; }
-	Node *Next(const Node *n) { return &n.m_next; }
-
-	//Find a node with the specified key
-	const Node *Find(const T &t) const { return (t == m_payload) ? &this : Find(*m_next->m_payload); }
-	Node *Find(const T &t) { return (t == m_payload) ? &this : Find(*m_next->m_payload); }
-
-	//Delete the given node
-	void Delete(Node *n){
-		delete n;
-	}
-
-
-	//Reverse the list in O(1) time
-	void Reverse() {
-
-	}
-
-		private:
-		//NOTE: you may add any private member variables or 
-		//      methods you need to complete the implementation
-		Node m_head = 0;             // Head node
-		int size = 0;
-
+  Node   *m_head;            // Front node
+  Node   *m_tail;            // Back node
+  bool   reversed;           // Tracks current direction of list
+  bool	 m_first;
+  Node   *n;
 };
 
 // Assignment operator.  Copies another list.
-template<class T> void CRevList<T>::operator=(const CRevList<T> &b){
-	
-	
+template<class T> void CRevList<T>::operator=(const CRevList<T> &b)
+{
+    Node *itr = b->m_head;
+      
+    while (itr != NULL)
+    {
+        PushBack(itr->m_payload);
+        
+        if (!b.IsReversed())
+        {
+            itr = itr->m_next;
+        }
+        
+        else
+        {
+            itr = itr->m_prev;
+        }
+    }
 }
 
-//template<class T> (CRevList<T>::Node *) CRevList<T>::Begin1() {return m_reverse ? m_head.m_prev : m_head.m_next;}
 
+
+//template<class T> (CRevList<T>::Node *) CRevList<T>::Begin1() {return m_reverse ? m_head.m_prev : m_head.m_next;}
 
 
 #endif
--- Linked List Project 2/main.cpp
+++ Linked List Project 2/main.cpp
@@ -10,7 +10,7 @@ using namespace std;
 int main(int argc, char* argv[]){
 
   if(argc != 3){
-    cerr << "Incorrect nubmer of command line arguments." << endl;
+    cerr << "Incorrect number of command line arguments." << endl;
     cerr << "\t[Prompt] <hotpotato> <number of players> <number of passes - or random number seed if negative>" << endl;
   }
 
@@ -21,10 +21,10 @@ int main(int argc, char* argv[]){
   //random number generator - borrowed from Avida - written by CA OFRIA and modified by J STEDWICK - based on RNG from Numerical Recipes in C: The Art of Scientific Computing (Flannery et al.)
   RandomNumberGenerator rng;
 
-  //are we using a random nubmer for m? if so seed with the absolute value of m
+  //are we using a random number for m? if so seed with the absolute value of m
   if(i_m < 0) rng.ResetSeed(i_m * -1); 
 
-  //reversable linked list that will hold the players
+  //reversible linked list that will hold the players
   CRevList<unsigned> ll_players;
 
   //set up the players
@@ -33,7 +33,7 @@ int main(int argc, char* argv[]){
   }
 
   //set up the game
-  unsigned u_steps = i_m;
+  /* unsigned u_steps = i_m;
 
   CRevList<unsigned>::Node *curr = ll_players.Begin();
   CRevList<unsigned>::Node *old = NULL;
@@ -43,21 +43,26 @@ int main(int argc, char* argv[]){
   //play the game
   while(!ll_players.IsEmpty()){
     if(i_m < 0) u_steps = rng.GetUInt(1,i_n+1);
-	old = *curr;
-	for(unsigned i=0; i<u_steps; i++){
-		curr = ll.players.Next(curr);
-	}
-	v_order.pushback(old.Data());
-	Delete(curr);
-	
-  }
+        
+    for (unsigned step = 0; step < u_steps; step++)
+    {
+        curr = ll_players.Next(curr); 
+    }
+	old = ll_players.Next(curr);
+    v_order.push_back(old->Data());
+    ll_players.Delete(curr);
+	old = curr;
+    ll_players.Reverse();
+    
+  } */
 
 
-  //ouptut the order
+  //output the order
   cout << "Final Order: ";
   for(unsigned i = 0; i < i_n; i++){
-    cout << v_order.at(i) << " ";
+    /* cout << v_order.at(i) << " "; */
   }
+  
   cout << endl;
 
 }
