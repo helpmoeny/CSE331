56b3d4f1bc5eed8d61d0688fd57a5b6a352c4298
Zachary <babylonnebula@gmail.com>
2015-10-16 19:16:13 -0400

    Finalized Tree.h

--- ZachTree.h
+++ ZachTree.h
@@ -1,3 +1,9 @@
+//
+// Name :         Tree.h
+// Description :  Simple binary search tree.
+// Created By :   Zachary Brenz
+//
+
 #ifndef CTREE_H
 #define CTREE_H
 
@@ -8,15 +14,15 @@ using namespace std;
 template<class T> class CTree
 {
 public:
-    CTree()
+    CTree() //Constructor for tree
     {
-        m_root = 0;
-        size = 0;
+        m_root = 0; //Set the root node to empty
+        size = 0; //Set the tree's size to 0
     }
     
-    ~CTree()
+    ~CTree() //Destructor for tree
     {
-        delete m_root;
+        delete m_root; //Delete the root node
     }
 
     //
@@ -43,43 +49,43 @@ public:
 
     };
 
-    void Insert(const T &t)
+    void Insert(const T &t) //Insert a value into the tree
     {
-        if (size == 0)
+        if (size == 0) //The tree is empty
         {
-            m_root = new Node(t);
+            m_root = new Node(t); //Create the root node with the given value
         }
         
-        else
+        else //If the tree is not empty
         {
-            Node *itr = m_root;
+            Node *itr = m_root; //Create an iterator node pointer
             
-            while (itr != nullptr)
+            while (itr != nullptr) //Search until the end of the tree is reached
             {
-                if (t < itr->m_number)
+                if (t < itr->m_number) //If the value being inserted is less than the current value
                 {
-                    if (itr->m_ll != nullptr)
+                    if (itr->m_ll != nullptr) //If the current node has a left leaf
                     {
-                        itr = itr->m_ll;
+                        itr = itr->m_ll; //Proceed left down the tree
                     }
                     
-                    else
+                    else //if the current node does not have a left leaf
                     {
-                        itr->m_ll = new Node(t);
+                        itr->m_ll = new Node(t); //Create a new node and insert it as the left leaf
                         break;
                     }
                 }
                         
-                else if (t > itr->m_number)
+                else if (t > itr->m_number) //If the value being inserted is greater than the current value
                 {
-                    if (itr->m_rl != nullptr)
+                    if (itr->m_rl != nullptr) //If the current node has a right leaf
                     {
-                        itr = itr->m_rl;
+                        itr = itr->m_rl; //Proceed right down the tree
                     }
                     
-                    else
+                    else //If the current node does not have a right leaf
                     {
-                        itr->m_rl = new Node(t);
+                        itr->m_rl = new Node(t); //Create a new node and insert it as the right leaf
                         break;
                     }
                 }
@@ -89,31 +95,30 @@ public:
         size++;
     }
     
-    void Preorder(std::ostream &p_str)
+    void Preorder(std::ostream &p_str) //Output the preorder traversal of the tree
     {
-        //p_str<<"[Preorder Goes Here]";
-        Node *n = m_root;
-        GetPreorder(p_str, n);
+        GetPreorder(p_str, m_root); //Call the preorder function
     }
     
-    int Depth()
+    int Depth() //Return the depth of the tree
     {
-        return GetDepth(m_root);
+        return GetDepth(m_root); //Call depth-calculating function
     }
 
-    const Node *Find(const T &t) const
+    const Node *Find(const T &t) const //Find a node in the tree
     {
-        return FindNode(t);
+        return FindNode(t); //Call find function
     }
-    Node *Find(const T &t)
+    
+    Node *Find(const T &t) //Find a node in the tree
     {
-        return FindNode(t);
+        return FindNode(t); //Call find function
     }
 
-    void Delete(const T &t)
+    void Delete(const T &t) //Delete a given value from the tree
     {
-        DeleteNode(m_root, t);
-        size--;
+        DeleteNode(m_root, t); //Call delete function
+        size--; //Decrement size of tree
     }
 
 
@@ -122,27 +127,32 @@ private:
     long size;
     
     
-    int GetDepth(Node *n)
+    int GetDepth(Node *n) //Get the depth of the tree
     {
-        if (n->m_ll == nullptr && n->m_rl == nullptr)
+        if (n == nullptr) //Tree is empty
+        {
+            return -1;
+        }
+        
+        if (n->m_ll == nullptr && n->m_rl == nullptr) //Tree has root but no leaves
         {
             return 0;
         }
         
-        else if (n->m_ll != nullptr && n->m_rl == nullptr)
+        else if (n->m_ll != nullptr && n->m_rl == nullptr) //Proceed left down tree if only left leaf exists
         {
             return GetDepth(n->m_ll) + 1;
         }
         
-        else if (n->m_ll == nullptr && n->m_rl != nullptr)
+        else if (n->m_ll == nullptr && n->m_rl != nullptr) //Proceed right down tree if only left leaf exists
         {
             return GetDepth(n->m_rl) + 1;
         }
         
-        else if (n->m_ll != nullptr && n->m_rl != nullptr)
+        else if (n->m_ll != nullptr && n->m_rl != nullptr) //Both leaves exist
         {
-            int left = GetDepth(n->m_ll), right = GetDepth(n->m_rl);
-            if (left > right)
+            int left = GetDepth(n->m_ll), right = GetDepth(n->m_rl); //Proceed down both sides of tree
+            if (left > right) //Return whichever side is deeper
             {
                 return left + 1;
             }
@@ -154,72 +164,72 @@ private:
         }
     }
     
-    Node *FindNode(const T &t)
+    Node *FindNode(const T &t) //Find and return a node in the tree
     {
-        Node *itr = m_root;
+        Node *itr = m_root; //Begin searching with root node
         
         while (itr != nullptr)
         {
-            if (itr->m_number == t)
+            if (itr->m_number == t) //Value has been found
             {
                 return itr;
             }
 
-            if (t < itr->m_number)
+            if (t < itr->m_number) //Current value is less than what is being searched for
             {
-                itr = itr->m_ll;
+                itr = itr->m_ll; //Proceed left down tree
             }
 
-            else if (t > itr->m_number)
+            else if (t > itr->m_number) //Current value is greater than what is being searched for
             {
-                itr = itr->m_rl;
+                itr = itr->m_rl; //Proceed right down tree
             }
         }
     }
     
-    void GetPreorder(std::ostream &p_str, Node *n)
+    void GetPreorder(std::ostream &p_str, Node *n) //Output the preorder traversal of the tree
     {
-        p_str<<n->m_number<<"  ";
+        p_str<<n->m_number<<"  "; //Output root node
         
-        if (n->m_ll != nullptr)
+        if (n->m_ll != nullptr) //If left leaf exists
         {
-            GetPreorder(p_str, n->m_ll);
+            GetPreorder(p_str, n->m_ll); //Begin preorder on left leaf
         }
         
-        if (n->m_rl != nullptr)
+        if (n->m_rl != nullptr) //If right leaf exists
         {
-            GetPreorder(p_str, n->m_rl);
+            GetPreorder(p_str, n->m_rl); //Begin preorder on right leaf
         }
     }
     
-    void DeleteNode(Node *n, const T &t)
+    void DeleteNode(Node *n, const T &t) //Remove a node from the tree
     {
-        Node *parent = nullptr;
-        Node *to_delete = n;
+        Node *parent = nullptr; //Parent of node to be deleted
+        Node *to_delete = n; //Node to be deleted
         
-        while (to_delete != nullptr)
+        while (to_delete != nullptr) //Until end of tree is reached
         {
-            if (to_delete->m_number == t)
+            if (to_delete->m_number == t) //Node to be deleted has been located
             {
                 break;
             }
             
             else
             {
-                if (t < to_delete->m_number)
+                if (t < to_delete->m_number) //Value being searched for is less than current value
                 {
                     parent = to_delete;
-                    to_delete = to_delete->m_ll;
+                    to_delete = to_delete->m_ll; //Proceed left down tree
                 }
 
-                else if (t > to_delete->m_number)
+                else if (t > to_delete->m_number) //Value being searched for is greater than current value
                 {
                     parent = to_delete;
-                    to_delete = to_delete->m_rl;
+                    to_delete = to_delete->m_rl; //Proceed right down tree
                 }
             }
             
-            if (to_delete == nullptr)
+            if (to_delete == nullptr) //Value was not found in tree
             {
                 return;
             }
@@ -229,80 +239,117 @@ private:
         
         if (to_delete->m_ll == nullptr && to_delete->m_rl == nullptr)
         {
-            if (parent != nullptr)
+            if (parent != nullptr) //to_delete is not the root node
             {
-                if (to_delete == parent->m_ll)
+                if (to_delete == parent->m_ll) //Deleting a left leaf
                 {
-                    parent->m_ll = nullptr;
+                    parent->m_ll = nullptr; //Remove pointer to to_delete
                     delete to_delete;
                 }
 
-                else if (to_delete == parent->m_rl)
+                else if (to_delete == parent->m_rl) //Deleting a right leaf
                 {
-                    parent->m_rl = nullptr;
+                    parent->m_rl = nullptr; //Remove pointer to to_delete
                     delete to_delete;
                 }
             }
             
-            else
+            else //to_delete is the root node
             {
+                m_root = nullptr; //Root node no longer exists
                 delete to_delete;
             }
         }
         
         //Delete Node with only left leaf
+        
         else if (to_delete->m_ll != nullptr && to_delete->m_rl == nullptr)
         {
-            if (parent != nullptr)
+            if (parent != nullptr) //to_delete is not the root node
             {
-                if (to_delete->m_number == parent->m_ll->m_number)
+                if (to_delete == parent->m_ll) //Deleting a left leaf
                 {
-                    parent->m_ll = to_delete->m_ll;
-                    delete to_delete;
+                    parent->m_ll = to_delete->m_ll; //Replace pointer to to_delete with its leaf
                 }
 
-                else if (to_delete->m_number == parent->m_rl->m_number)
+                else if (to_delete == parent->m_rl) //Deleting a right leaf
                 {
-                    parent->m_rl = to_delete->m_ll;
-                    delete to_delete;
+                    parent->m_rl = to_delete->m_ll; //Replace pointer to to_delete with its leaf
                 }
             }
             
-            else
+            else //to_delete is the root node
             {
-                delete to_delete;
+                m_root = m_root->m_ll; //Set root node to left leaf
             }
         }
-        
         
         //Delete Node with only right leaf
         else if (to_delete->m_ll == nullptr && to_delete->m_rl != nullptr)
         {
-            if (parent != nullptr)
+            if (parent != nullptr) //to_delete is not the root node
             {
-                if (to_delete->m_number == parent->m_ll->m_number)
+                if (to_delete == parent->m_ll) //Deleting a left leaf
                 {
-                    parent->m_ll = to_delete->m_rl;
-                    delete to_delete;
+                    parent->m_ll = to_delete->m_rl; //Replace pointer to to_delete with its leaf
                 }
 
-                else if (to_delete->m_number == parent->m_rl->m_number)
+                else if (to_delete == parent->m_rl) //Deleting a right leaf
                 {
-                    parent->m_rl = to_delete->m_rl;
-                    delete to_delete;
+                    parent->m_rl = to_delete->m_rl; //Replace pointer to to_delete with its leaf
                 }
             }
             
-            else
+            else //to_delete is the root node
             {
-                delete to_delete;
+                m_root = m_root->m_rl; //Set root node to right leaf
             }
         }
         
+        
         //Delete Node with left and right leaf
+        
         else if (to_delete->m_ll != nullptr && to_delete->m_rl != nullptr)
         {
+            parent = to_delete; //parent becomes the node to be deleted
+            to_delete = to_delete->m_rl; //to_delete becomes its right leaf
             
+            if (to_delete->m_ll == nullptr && to_delete->m_rl == nullptr) //The right leaf has no leaves
+            {
+                 parent->m_number = to_delete->m_number; //Move the data from to_delete into parent's position
+                 delete to_delete; //Delete to_delete
+                 parent->m_rl = nullptr; //Remove the pointer to to_delete
+            }
+            
+            else if (to_delete->m_ll != nullptr) //If the right leaf has a left leaf
+            {
+                Node *new_leaf = to_delete->m_ll; //Creates a pointer to the new left leaf of to_delete
+                
+                while (new_leaf->m_ll != nullptr) //Proceeds down the tree and to the left, moving all of the leaves up by one slot
+                {
+                    to_delete = new_leaf;
+                    new_leaf = new_leaf->m_ll;
+                }
+                
+                parent->m_number = new_leaf->m_number; //Moves new_leaf's data to parent's position
+                delete new_leaf; //Deletes new_leaf
+                to_delete->m_ll = nullptr; //Removes the pointer to new_leaf
+            }
+            
+            else //The right leaf has a right leaf
+            {
+                Node *new_leaf = to_delete->m_rl; //Creates a pointer to the new right leaf of to_delete
+                
+                while (new_leaf->m_rl != nullptr) //Proceeds down the tree and to the right, moving all of the leaves up by one slot
+                {
+                    to_delete = new_leaf;
+                    new_leaf = new_leaf->m_rl;
+                }
+                
+                parent->m_number = new_leaf->m_number; //Moves new_leaf's data to parent's position
+                delete new_leaf; //Deletes new_leaf
+                to_delete->m_rl = nullptr; //Removes the pointer to new_leaf
+            }
         }
     }
     
